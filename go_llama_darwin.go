6a22ec67f102267c4a9f77b8e0616fcfaac5e255
Stanimir Dimitrov <stanchino@gmail.com>
2024-04-20 15:08:22 +0300

    chore: remove darwing build file

--- go_llama_darwin.go
+++
@@ -1,9 +0,0 @@
-//go:build darwin
-// +build darwin
-
-package llama
-
-/*
-#cgo darwin LDFLAGS: -L${SRCDIR}/lib -lllama_arm64 -framework Accelerate -framework Foundation -framework Metal -framework MetalKit -framework MetalPerformanceShaders
-*/
-import "C"
