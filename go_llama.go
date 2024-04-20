68322d5cf7b8b7cd7ba9cbf981125113e69840ad
Stanimir Dimitrov <stanchino@gmail.com>
2024-04-20 16:00:14 +0300

    chore: change arch for cgo

--- go_llama.go
+++ go_llama.go
@@ -4,7 +4,7 @@ package llama
 #cgo CFLAGS: -std=c11 -I${SRCDIR}/includes/
 #cgo CXXFLAGS: -std=c++11 -I${SRCDIR}/includes/
 #cgo darwin,arm64 LDFLAGS: -L${SRCDIR}/lib -lllama_arm64 -framework Accelerate -framework Foundation -framework Metal -framework MetalKit -framework MetalPerformanceShaders
-#cgo linux,x86_64 LDFLAGS: -L${SRCDIR}/lib -lllama_x86_64 -lpthread
+#cgo linux,amd64 LDFLAGS: -L${SRCDIR}/lib -lllama_x86_64 -lpthread
 #include <stdlib.h>
 #include <stdbool.h>
 #include "go_llama.h"
